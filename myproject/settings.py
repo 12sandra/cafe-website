f289a10b0201bf036b8fb147a8aef8c712f7d96d
Sandra <sandrasunilkumar4860@gmail.com>
2026-01-29 01:25:21 +0530

    Database connected and migrations ready

--- myproject/settings.py
+++ myproject/settings.py
@@ -103,9 +103,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
-        conn_max_age=600,
-        ssl_require=True if os.environ.get('DATABASE_URL') else False
+        default=os.environ.get('DATABASE_URL')
     )
 }
 # Password validation
